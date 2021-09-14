4767e14d424db373165a5076c2e028a220a54114
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2021-09-13 20:13:21 -0700

    Remove src/.temp and set gitignore

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Alan’s GitHub Pages",
-  "siteUrl": "https://alanjcfs.github.io",
-  "version": "0.7.23",
-  "catchLinks": true
-}
